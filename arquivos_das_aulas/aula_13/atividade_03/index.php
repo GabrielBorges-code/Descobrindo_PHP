eb89d8d3ae710f27d2c03ee639535d42bfbb0409
Gabriel Borges <gabriel.moura2009@hotmail.com>
2020-12-21 21:40:29 -0300

    atividade 13 okay

---
+++ arquivos_das_aulas/aula_13/atividade_03/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../../_css/estilo.css">
+
+    <title>Aula 12</title>
+</head>
+<body>
+
+    <div>
+        <form action="primo.php" method="get">
+            <fieldset><legend>Nùmero primo</legend>
+
+                numero: <input type="number" name="num" id="num"> <input type="submit" value="calcular">
+           
+            </fieldset>
+
+        </form>
+    </div>
+    
+</body>
+</html>
