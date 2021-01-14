8b59f67b95c7aeac5315ec147ea5956a4dd60b40
Gabriel B <gabriel.moura2009@hotmail.com>
2021-01-14 20:40:04 -0300

    modulo1_workshop

---
+++ webserie_php/01_iniciando/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>FSPH: Iniciando um projeto </title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Source+Code+Pro:wght@200&display=swap" rel="stylesheet">
+
+   <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    
+    <?php
+        //Bloco PHP com variável
+        $start = "Vamos começar"; 
+        echo "<h1>Olá mundo! {$start}</h1>";
+        echo "<p id='js'>Loding...</p>";
+    ?>
+
+  <script src="assets/script.js"></script>
+
+</body>
+</html>
